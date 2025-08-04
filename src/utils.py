8c473d6d2a47ec5d49472f3a3e3a145a037546ac
Van Datt <dovandat2614@gmail.com>
2025-08-04 13:47:41 +0700

    fix

--- src/utils.py
+++ src/utils.py
@@ -5,7 +5,7 @@ import numpy as np
 def process_img(obs):
 
     # img_state = cv2.resize(obs, (84, 84), interpolation=cv2.INTER_AREA)
-    img_state = img_state.reshape((obs.shape[0], obs.shape[1], 1))
+    img_state = obs.reshape((obs.shape[0], obs.shape[1], 1))
 
     img_tensor = img_state.transpose(2, 0, 1).astype(np.float32)
     img_tensor = torch.from_numpy(img_tensor)
